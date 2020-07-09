aa0554180dbd2b504475c2c79e4d3bfa61a427ff
d-beloved <morayodeji@gmail.com>
2020-07-09 22:46:30 +0100

    Create save video to local storage

--- App.js
+++ App.js
@@ -1,99 +1,110 @@
-import * as ImagePicker from 'expo-image-picker';
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, Image, Share, Clipboard } from "react-native";
+import * as MediaLibrary from "expo-media-library";
 import * as Permissions from 'expo-permissions';
-import * as firebase from 'firebase';
-import React from 'react';
-import {
-  ActivityIndicator,
-  Button,
-  Clipboard,
-  Image,
-  Share,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import uuid from 'uuid';
+import { Camera } from 'expo-camera';
+import * as firebase from "firebase";
+import uuid from "uuid";
+
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAlZruO2T_JNOWn4ysfX6AryR6Dzm_VVaA',
-  authDomain: 'blobtest-36ff6.firebaseapp.com',
-  databaseURL: 'https://blobtest-36ff6.firebaseio.com',
-  storageBucket: 'blobtest-36ff6.appspot.com',
-  messagingSenderId: '506017999540',
+  apiKey: "AIzaSyBxeBLfG8bFfyg5wwft0stNdEMRM6OPjBo",
+  authDomain: "videoupload-f4879.firebaseapp.com",
+  databaseURL: "https://videoupload-f4879.firebaseio.com",
+  projectId: "videoupload-f4879",
+  storageBucket: "videoupload-f4879.appspot.com",
+  messagingSenderId: "659565983703",
+  appId: "1:659565983703:web:7376471096873bb1a07d92",
+  measurementId: "G-J8SSTPW9KS",
 };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-// Firebase sets some timeers for a long period, which will trigger some warnings. Let's turn that off for this example
+// Firebase sets some timers for a long period, which will trigger some warnings. Let's turn that off for this example
 console.disableYellowBox = true;
 
-export default class App extends React.Component {
+class MyCam extends Component {
   state = {
-    image: null,
+    video: null,
+    recording: false,
     uploading: false,
   };
 
-  async componentDidMount() {
-    await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    await Permissions.askAsync(Permissions.CAMERA);
-  }
+  // _saveVideo = async () => {
+  //   const { video } = this.state;
+  //   const asset = await MediaLibrary.createAssetAsync(video.uri);
+  //   if (asset) {
+  //     this.setState({ video: null });
+  //   }
+  // };
 
-  render() {
-    let { image } = this.state;
+  _StopRecord = async () => {
+    this.setState({ recording: false }, () => {
+      this.cam.stopRecording();
+    });
+  };
 
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        {!!image && (
-          <Text
-            style={{
-              fontSize: 20,
-              marginBottom: 20,
-              textAlign: 'center',
-              marginHorizontal: 15,
-            }}>
-            Example: Upload ImagePicker result
-          </Text>
-        )}
+  _StartRecord = async () => {
+    if (this.cam) {
+      this.setState({ recording: true }, async () => {
+        const video = await this.cam.recordAsync({ maxDuration: 15 });
+        // this.setState({ video });
+        this._StopRecord();
+        this._handleRecordedVideo(video);
+      });
+    }
+  };
 
-        <Button
-          onPress={this._pickImage}
-          title="Pick an image from camera roll"
-        />
+  toogleRecord = () => {
+    const { recording } = this.state;
 
-        <Button onPress={this._takePhoto} title="Take a photo" />
+    if (recording) {
+      this._StopRecord();
+    } else {
+      this._StartRecord();
+    }
+  };
 
-        {this._maybeRenderImage()}
-        {this._maybeRenderUploadingOverlay()}
+  _handleRecordedVideo = async (pickerResult) => {
+    try {
+      this.setState({ uploading: true });
 
-        <StatusBar barStyle="default" />
-      </View>
-    );
-  }
+      if (!pickerResult.cancelled) {
+        uploadUrl = await uploadVideoAsync(pickerResult.uri);
+        this.setState({ video: uploadUrl });
+      }
+    } catch (e) {
+      console.log(e);
+      alert("Upload failed, sorry :(");
+    } finally {
+      this.setState({ uploading: false });
+    }
+  };
 
-  _maybeRenderUploadingOverlay = () => {
+  _renderUploadingOverlay = () => {
     if (this.state.uploading) {
       return (
         <View
           style={[
             StyleSheet.absoluteFill,
             {
-              backgroundColor: 'rgba(0,0,0,0.4)',
-              alignItems: 'center',
-              justifyContent: 'center',
+              backgroundColor: "rgba(0,0,0,0.4)",
+              alignItems: "center",
+              justifyContent: "center",
             },
-          ]}>
+          ]}
+        >
           <ActivityIndicator color="#fff" animating size="large" />
         </View>
       );
     }
   };
 
-  _maybeRenderImage = () => {
-    let { image } = this.state;
-    if (!image) {
+  _maybeRenderVideo = () => {
+    let { video } = this.state;
+    if (!video) {
       return;
     }
 
@@ -104,24 +115,27 @@ export default class App extends React.Component {
           width: 250,
           borderRadius: 3,
           elevation: 2,
-        }}>
+        }}
+      >
         <View
           style={{
             borderTopRightRadius: 3,
             borderTopLeftRadius: 3,
-            shadowColor: 'rgba(0,0,0,1)',
+            shadowColor: "rgba(0,0,0,1)",
             shadowOpacity: 0.2,
             shadowOffset: { width: 4, height: 4 },
             shadowRadius: 5,
-            overflow: 'hidden',
-          }}>
-          <Image source={{ uri: image }} style={{ width: 250, height: 250 }} />
+            overflow: "hidden",
+          }}
+        >
+          <Image source={{ uri: video }} style={{ width: 250, height: 250 }} />
         </View>
         <Text
           onPress={this._copyToClipboard}
           onLongPress={this._share}
-          style={{ paddingVertical: 10, paddingHorizontal: 10 }}>
-          {image}
+          style={{ paddingVertical: 10, paddingHorizontal: 10 }}
+        >
+          {video}
         </Text>
       </View>
     );
@@ -129,73 +143,119 @@ export default class App extends React.Component {
 
   _share = () => {
     Share.share({
-      message: this.state.image,
-      title: 'Check out this photo',
-      url: this.state.image,
+      message: this.state.video,
+      title: "Check out this video",
+      url: this.state.video,
     });
   };
 
   _copyToClipboard = () => {
-    Clipboard.setString(this.state.image);
-    alert('Copied image URL to clipboard');
+    Clipboard.setString(this.state.video);
+    alert("Copied video URL to clipboard");
   };
 
-  _takePhoto = async () => {
-    let pickerResult = await ImagePicker.launchCameraAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-    });
-
-    this._handleImagePicked(pickerResult);
-  };
-
-  _pickImage = async () => {
-    let pickerResult = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-    });
-
-    this._handleImagePicked(pickerResult);
-  };
-
-  _handleImagePicked = async pickerResult => {
-    try {
-      this.setState({ uploading: true });
-
-      if (!pickerResult.cancelled) {
-        uploadUrl = await uploadImageAsync(pickerResult.uri);
-        this.setState({ image: uploadUrl });
-      }
-    } catch (e) {
-      console.log(e);
-      alert('Upload failed, sorry :(');
-    } finally {
-      this.setState({ uploading: false });
-    }
-  };
+  render() {
+    const { recording } = this.state;
+    return (
+      <Camera
+        ref={cam => (this.cam = cam)}
+        style={{
+          justifyContent: "flex-end",
+          alignItems: "center",
+          flex: 1,
+          width: "100%"
+        }}
+      >
+        {/* {video && (
+          <TouchableOpacity
+            onPress={this._saveVideo}
+            style={{
+              padding: 20,
+              width: "100%",
+              backgroundColor: "#fff"
+            }}
+          >
+            <Text style={{ textAlign: "center" }}>Upload recording</Text>
+          </TouchableOpacity>
+        )} */}
+        <TouchableOpacity
+          onPress={this.toogleRecord}
+          style={{
+            padding: 20,
+            width: "100%",
+            backgroundColor: recording ? "#ef4f84" : "#4fef97"
+          }}
+        >
+          <Text style={{ textAlign: "center" }}>
+            {recording ? "Stop" : "Record"}
+          </Text>
+        </TouchableOpacity>
+        {this._maybeRenderVideo()}
+        {this._renderUploadingOverlay()}
+      </Camera>
+    );
+  }
 }
 
-async function uploadImageAsync(uri) {
+export default class RecVideo extends Component {
+  state = {
+    showCamera: false,
+    audioRecord: false,
+  };
+
+  _showCamera = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    const { status2 } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+
+    if (status === "granted") {
+      this.setState({ showCamera: true });
+    }
+  };
+
+  render() {
+    const { showCamera } = this.state;
+    return (
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          flex: 1,
+          width: "100%",
+        }}
+      >
+        {showCamera ? (
+          <MyCam />
+        ) : (
+          <TouchableOpacity
+            onPress={this._showCamera}
+            style={{ backgroundColor: "red", padding: 15, borderRadius: 50 }}
+          >
+            <Text style={{ fontSize:18, fontWeight: 'bold', color: 'white' }}> Record video (max 15secs) </Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    );
+  }
+}
+
+async function uploadVideoAsync(uri) {
   // Why are we using XMLHttpRequest? See:
   // https://github.com/expo/expo/issues/2402#issuecomment-443726662
   const blob = await new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-    xhr.onload = function() {
+    xhr.onload = function () {
       resolve(xhr.response);
     };
-    xhr.onerror = function(e) {
+    xhr.onerror = function (e) {
       console.log(e);
-      reject(new TypeError('Network request failed'));
+      reject(new TypeError("Network request failed"));
     };
-    xhr.responseType = 'blob';
-    xhr.open('GET', uri, true);
+    xhr.responseType = "blob";
+    xhr.open("GET", uri, true);
     xhr.send(null);
   });
 
-  const ref = firebase
-    .storage()
-    .ref()
-    .child(uuid.v4());
+  const ref = firebase.storage().ref().child(uuid.v4());
   const snapshot = await ref.put(blob);
 
   // We're done with the blob, close and release it
